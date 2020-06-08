ced898aab7c90b03a64be65eda9353459154588a
Jack-Hsieh <63274027+Jack-Hsieh@users.noreply.github.com>
2020-06-08 15:27:50 +0800

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -174,7 +174,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'collect_static/')
 
 #config/settings/py
 # Login redirect to home page:
